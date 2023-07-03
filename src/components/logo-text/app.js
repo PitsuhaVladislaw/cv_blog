ae48035827d35fd59e1a385340bb5990e549b110
Vladislaw <vladikpizza02@gmail.com>
2023-07-03 23:56:56 +0300

    feat: add files and settings

--- src/components/logo-text/app.js
+++ src/components/logo-text/app.js
@@ -1,6 +1,7 @@
 const floorWork = document.getElementById("floor-work");
 const textIcon = document.getElementById("text-icon");
 const screenImage = document.getElementById("scr-img");
+const workSecond = document.querySelector(".work-screen");
 
 const butClose = document.getElementById("butExpeClose");
 const butOpen = document.getElementById("butExpeOpen");
@@ -9,8 +10,35 @@ const butClCheck = document.getElementById("checkClBut");
 
 butOpen.addEventListener("click", activeIcon);
 butClose.addEventListener("click", closedIcon);
-butOpCheck.addEventListener("click", usualBlock);
-butClCheck.addEventListener("click", dedicateBlock);
+//butOpCheck.addEventListener("click", usualBlock);
+//butClCheck.addEventListener("click", dedicateBlock);
+
+workSecond.addEventListener("click", usualBlock);
+//workSecond.addEventListener("click", dedicateBlock);
+
+function closedIcon(e) {
+    e.preventDefault();
+
+    textIcon.classList.remove("text-icon");
+    textIcon.style.display = "none";
+
+    setTimeout(function() {
+        floorWork.classList.remove("floor-work-open");
+        floorWork.classList.toggle("floor-work-closed");
+    }, 100)
+}
+
+function activeIcon(e) {
+    e.preventDefault();
+
+    floorWork.classList.remove("floor-work-closed");
+    floorWork.classList.add("floor-work-open");
+
+    setTimeout(function() {
+        textIcon.classList.add("text-icon");
+        textIcon.style.display = "flex";
+    }, 1000)
+}
 
 function usualBlock(e) {
     e.preventDefault();
@@ -46,28 +74,4 @@ function dedicateBlock(e) {
     setTimeout(function() {
         activeIcon(e);
     }, 1500);
-}
-
-function closedIcon(e) {
-    e.preventDefault();
-
-    textIcon.classList.remove("text-icon");
-    textIcon.style.display = "none";
-
-    setTimeout(function() {
-        floorWork.classList.remove("floor-work-open");
-        floorWork.classList.toggle("floor-work-closed");
-    }, 100)
-}
-
-function activeIcon(e) {
-    e.preventDefault();
-
-    floorWork.classList.remove("floor-work-closed");
-    floorWork.classList.add("floor-work-open");
-
-    setTimeout(function() {
-        textIcon.classList.add("text-icon");
-        textIcon.style.display = "flex";
-    }, 1000)
 }
