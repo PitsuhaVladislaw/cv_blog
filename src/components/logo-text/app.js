dcfe57d8130db882aaf36f765da84c8e461f2679
Vladislaw <vladikpizza02@gmail.com>
2023-10-30 18:02:29 +0100

    fix: fix mistakes

--- src/components/logo-text/app.js
+++ src/components/logo-text/app.js
@@ -10,68 +10,15 @@ const butClCheck = document.getElementById("checkClBut");
 
 butOpen.addEventListener("click", activeIcon);
 butClose.addEventListener("click", closedIcon);
-//butOpCheck.addEventListener("click", usualBlock);
-//butClCheck.addEventListener("click", dedicateBlock);
-
-workSecond.addEventListener("click", usualBlock);
-//workSecond.addEventListener("click", dedicateBlock);
-
-function closedIcon(e) {
-    e.preventDefault();
-
-    textIcon.classList.remove("text-icon");
-    textIcon.style.display = "none";
-
-    setTimeout(function() {
-        floorWork.classList.remove("floor-work-open");
-        floorWork.classList.toggle("floor-work-closed");
-    }, 100)
-}
 
 function activeIcon(e) {
-    e.preventDefault();
+    e.prefentDefault();
 
-    floorWork.classList.remove("floor-work-closed");
-    floorWork.classList.add("floor-work-open");
-
-    setTimeout(function() {
-        textIcon.classList.add("text-icon");
-        textIcon.style.display = "flex";
-    }, 1000)
-}
-
-function usualBlock(e) {
-    e.preventDefault();
-
-    closedIcon(e);
-
-    const changeImage = (elem) => {
-        elem.preventDefault();
-
-        screenImage.classList.remove("work-screen-open");
-        screenImage.classList.add("work-screen-close");
+    () => {
+        screenImage
     }
-
-    setTimeout(function() {
-        changeImage(e)
-    }, 1500) ;
 }
 
-function dedicateBlock(e) {
-    e.preventDefault();
-
-    const changeImage = (elem) => {
-        elem.preventDefault();
-
-        screenImage.classList.remove("work-screen-close");
-        screenImage.classList.add("work-screen-open");
-    } 
-
-    setTimeout(function() {
-        changeImage(e);
-    }, 500);
-
-    setTimeout(function() {
-        activeIcon(e);
-    }, 1500);
+function closedIcon(e) {
+    e.prefentDefault();
 }
